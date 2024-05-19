2abbbba03b06547244592b0e0de42bc95a65178c
Nguyễn Quốc Huy <huyhakwalker@gmail.com>
2024-05-19 22:06:42 +0700

    newupdate

--- HomeStayWeb/HomeStayWeb/Controllers/HomeController.cs
+++ HomeStayWeb/HomeStayWeb/Controllers/HomeController.cs
@@ -26,5 +26,11 @@ namespace HomeStayWeb.Controllers
 
             return View();
         }
+        public ActionResult Services()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# DoAnUngDungWeb
+[# DoAnUngDungWeb](https://docs.google.com/document/d/1V8VpYkfvvmv2r-HdD_vPOODiOYjVjVlZyFLFaBnZts8/edit?pli=1)
