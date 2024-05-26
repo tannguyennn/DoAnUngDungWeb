0dcb7a6fc776f3b17b36f9bdd32ddb94acea5ce4
Nguyễn Quốc Huy <huyhakwalker@gmail.com>
2024-05-26 21:42:08 +0700

    áđâsda

--- HomeStayWeb/HomeStayWeb/Controllers/HomeController.cs
+++ HomeStayWeb/HomeStayWeb/Controllers/HomeController.cs
@@ -34,5 +34,69 @@ namespace HomeStayWeb.Controllers
 
             return View();
         }
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string email, string password)
+        {
+
+            var data = db.NhanViens.Where(s => s.EmailNV.Equals(email) && s.MKNV.Equals(password));
+            if (data.Count() > 0)
+            {
+                //add session
+                Session["tennv"] = data.FirstOrDefault().TenNV;
+                Session["emailnv"] = data.FirstOrDefault().EmailNV;
+                Session["id"] = data.FirstOrDefault().IDNV;
+                return RedirectToAction("Index", "Admin/NhanViens");
+            }
+            var data2 = db.KhachHangs.Where(s => s.EmailKH.Equals(email) && s.MKKH.Equals(password));
+            if (data2.Count() > 0)
+            {
+                //add session
+                Session["tenkh"] = data2.FirstOrDefault().TenKH;
+                Session["emailkh"] = data2.FirstOrDefault().EmailKH;
+                Session["id"] = data2.FirstOrDefault().IDKH;
+                Session["sdtkh"] = data2.FirstOrDefault().SDTKH;
+                return RedirectToAction("Index", "Home");
+            }
+            ViewBag.error = "Tên đăng nhập hoặc mật khẩu không đúng";
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //POST: Register
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(KhachHang _user)
+        {
+            if (ModelState.IsValid)
+            {
+                var check = db.KhachHangs.FirstOrDefault(s => s.EmailKH == _user.EmailKH);
+                if (check == null)
+                {
+                    db.Configuration.ValidateOnSaveEnabled = false;
+                    db.KhachHangs.Add(_user);
+                    db.SaveChanges();
+                    Session["tenkh"] = _user.TenKH;
+                    Session["emailkh"] = _user.EmailKH;
+                    Session["id"] = _user.IDKH;
+                    ViewBag.SuccessMessage = "Đăng ký thành công!";
+                    return RedirectToAction("Login", "Home");
+                }
+                else
+                {
+                    ViewBag.error = "Email already exists";
+                    return View();
+                }
+            }
+            return View();
+        }
     }
 }
